101d924bd889454d0bcd1bf9f443fe69d9a511d7
xDShot <miha-xdshot@yandex.ru>
2016-08-09 18:52:40 +0300

    generate pointshop items

--- tools/generate_lua.py
+++ tools/generate_lua.py
@@ -6,18 +6,24 @@ import argparse
 import ast
 
 parser = argparse.ArgumentParser(description='Automatically generate lua files using template and list of knives. Don\'t waste time on editing each lua, do it at once!')
-parser.add_argument('-t', default='template.txt', help='template file')
+parser.add_argument('-wt', default='weapon_template.txt', help='weapon template file')
+parser.add_argument('-wd', default='../lua/weapons/', help='where to save weapons lua files')
+parser.add_argument('-pt', default='pointshop_template.txt', help='pointshop item template file')
+parser.add_argument('-pd', default='../lua/pointshop/items/weapons/', help='where to save pointshop items lua files')
 parser.add_argument('-l', default='kniveslist.txt', help='list file')
-parser.add_argument('-d', default='../lua/weapons/', help='where to save lua files')
 parser.add_argument('-b', default='csgo_baseknife', help='name of base class')
 
 args = parser.parse_args()
 
-Template_file	= io.open(args.t, 'r')
-Template_text	= Template_file.read()
-Template_file.close()
+Weapon_file		= io.open(args.wt, 'r')
+Weapon_text		= Weapon_file.read()
+Weapon_file.close()
+Pointshop_file	= io.open(args.pt, 'r')
+Pointshop_text	= Pointshop_file.read()
+Pointshop_file.close()
 List_file		= io.open(args.l, 'r')
-Lua_dir			= args.d
+Weapon_dir		= args.wd
+Pointshop_dir	= args.pd
 Base_name		= args.b
 
 line_number = 0
@@ -52,12 +58,19 @@ for line in List_file:
 				'equipmenu':Equip_Menu,
 				'canbuy':Can_buy}
 	
-	output_text = Template_text.format(**Format)
+	output_text = Weapon_text.format(**Format)
 	print(output_text)
 	
-	output_file = open(Lua_dir + Lua_name + '.lua', 'w')
+	output_file = open(Weapon_dir + Lua_name + '.lua', 'w')
 	output_file.write(output_text)
 	output_file.close()
 	
+	output_text = Pointshop_text.format(**Format)
+	print(output_text)
+	
+	output_file = open(Pointshop_dir + Lua_name + '.lua', 'w')
+	output_file.write(output_text)
+	output_file.close()
+
 #don't forget to close all remaining files
 List_file.close()
